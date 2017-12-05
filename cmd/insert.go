41d15485f49df6459974bcc486399230ce9c2872
Eiso Kant <contact@eisokant.com>
2017-12-05 14:42:10 +0100

    minor fix

--- cmd/insert.go
+++ cmd/insert.go
@@ -40,7 +40,11 @@ func (c *InsertCmd) Execute(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	p := encrypt.NewPGP(pk, f, true)
+	p := encrypt.NewPGP(pk, f, false)
+
+	if err := p.Encrypt(); err != nil {
+		return err
+	}
 
 	if err := r.Load(); err != nil {
 		return err
